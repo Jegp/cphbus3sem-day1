f6ab6060af0b45888f7824e58c900b3a56c1c904
Jens Egholm <jens@selftiesoftware.com>
2017-01-27 00:40:56 +0100

    Corrected last task to use a supplier

--- src/main/java/Lambda.java
+++ src/main/java/Lambda.java
@@ -2,18 +2,20 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.concurrent.Callable;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * <h2>Introduction exercise on lambdas.</h2>
  * <p>
- * To complete this exercise, implement the three methods below
+ * To complete this exercise, implement the three methods below.
  * </p>
  * <h2>Maven guide</h2>
- * <p>When you're done, try runnig the maven 'verify' task
- * and go to <a href="http://46.101.113.24:6000/">http://46.101.113.24:6000/{your user name}</a> where
+ * <p>When you're done, try runnig the maven 'verify' task either from Netbeans or by going to a console and typing
+ * 'mvn verify'. When that succeeds go to
+ * <a href="http://46.101.113.24:6000/">http://46.101.113.24:6000/{your user name}</a> where
  * "{your user name}" is your name of your Windows/Mac/Unix user (it will feature in the console output when deploying
  * through maven. If this gives you any problems (if your username is ⚔ for instance), please change the name in
- * the pom.xml file under the <code><user></user></code> tags.
+ * the pom.xml file under the <code>properties -> user</code> tag.
  * </p>
  */
 public class Lambda {
@@ -36,8 +38,8 @@ public class Lambda {
     /**
      * <h2>Task 2</h2>
      * Now you're given a lambda (in this case a {@link Callable}). This lambda is a producer (we'll talk more about
-     * those later) which can produce numbers for you. Your task is simply to produce such a number from the
-     * producer and return it.
+     * those later in the semester) which generates numbers for you. One call to the producer = one number. Your task
+     * is simply to get such a number from the producer and return it.
      *
      * @param producer A producer which can generate numbers.
      * @return A number generated from the producer.
@@ -48,15 +50,16 @@ public class Lambda {
 
     /**
      * <h2>Task 3</h2>
-     * Now you're given a {@link Function} which performs some magic you are not aware of. Your task is to execute
-     * that function three times with the output from {@link Lambda#produce(Callable)}. So first get the output from
-     * {@link #produce(Callable)}, then call the input function that input, store the result and repeat two more times.
-     * The result should be returned.
+     * Now you're given a {@link Function} and a producer ({@link java.util.function.Supplier}). The supplier gives
+     * you a number and the function performs some magic you are not aware of. Your task is to execute
+     * that function three times with the number from the producer. First get the number, then apply the function
+     * to the number, store the result and repeat the application two more times. The result should be returned.
      *
      * @param function A function which performs some magic on a number.
+     * @param supplier Supplies numbers for the calculation.
      * @return The function applied three times
      */
-    public static int triple(Function<Integer, Integer> function) {
+    public static int triple(Function<Integer, Integer> function, Supplier<Integer> supplier) {
         throw new NotImplementedException();
     }
 
